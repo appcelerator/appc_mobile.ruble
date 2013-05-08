96f4604b7edd4fe03cb179517b99f5da3cb593dc
Nam Le <nle@appcelerator.com>
2013-05-08 16:05:29 -0700

    Initial commit

---
+++ bundle.rb
@@ -0,0 +1,9 @@
+require 'ruble'
+
+bundle 'Appcelerator Platform' do |bundle|
+  bundle.author = 'Thomas Aylott/Appcelerator'
+  bundle.copyright = "Copyright 2011 Thomas Aylott/Appcelerator. Distributed under the MIT license."
+  bundle.description = t(:bundle_description)
+  bundle.display_name = t(:bundle_name)
+  bundle.repository = "git://github.com/appcelerator/appc_mobile.ruble.git"  
+end
