4bd5c9820a097e65ae023298b960813c05be904e
Nam Le <nle@appcelerator.com>
2013-05-09 17:16:33 -0700

    Update repo for the field services app

--- samples/project_samples.rb
+++ samples/project_samples.rb
@@ -3,7 +3,7 @@ require 'ruble'
 project_sample t(:field_service) do |s|
   s.id = "com.appcelerator.titanium.appc.mobile.samples.mapping"
   s.category = "com.appcelerator.titanium.appc.mobile.samples.category"
-  s.location = "git://github.com/appcelerator/Field-Service-App-Mobile.git"
+  s.location = "git://github.com/appcelerator/Field-Service-App.git"
   s.description = t(:field_service_description)
   s.natures = ["com.appcelerator.titanium.alloy.core.nature","com.appcelerator.titanium.mobile.nature", "com.aptana.projects.webnature"]
   s.icon = "http://preview.appcelerator.com/dashboard/img/icons/icon_geo.png"
