3702f9a4e0789250e7ea89c980ddc0ee8cd5d45c
Nam Le <nle@appcelerator.com>
2013-05-09 17:16:33 -0700

    Update repo for the field services app

--- bundle.rb
+++ bundle.rb
@@ -1,8 +1,8 @@
 require 'ruble'
 
 bundle 'Appcelerator Platform' do |bundle|
-  bundle.author = 'Thomas Aylott/Appcelerator'
-  bundle.copyright = "Copyright 2011 Thomas Aylott/Appcelerator. Distributed under the MIT license."
+  bundle.author = 'Appcelerator, Appcelerator Titanium and associated marks and logos are trademarks of Appcelerator, Inc.'
+  bundle.copyright = "Titanium is Copyright (c) 2008-2013 by Appcelerator, Inc. All Rights Reserved."
   bundle.description = t(:bundle_description)
   bundle.display_name = t(:bundle_name)
   bundle.repository = "git://github.com/appcelerator/appc_mobile.ruble.git"  
